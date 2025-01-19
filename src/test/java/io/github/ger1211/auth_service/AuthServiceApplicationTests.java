49c3b45ddeb630e2063b252cb645671aa9a17581
Germán Maximiliano Cabrera <gmcabrera121190@gmail.com>
2025-01-19 12:02:45 -0300

    Initial commit.

---
+++ src/test/java/io/github/ger1211/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ger1211.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
